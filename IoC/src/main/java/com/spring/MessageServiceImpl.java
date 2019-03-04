e367d9922a8f7655a35951d982834d434bf6dec0
tyttpe <994148249@qq.com>
2019-03-04 10:04:51 +0800

    modul IoC

---
+++ IoC/src/main/java/com/spring/MessageServiceImpl.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+public class MessageServiceImpl implements MessageService{
+    private String username;
+    private int age;
+
+    public MessageServiceImpl(String username,int age){
+        this.username=username;
+        this.age=age;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Hello World!"+username+", age is "+ age;
+    }
+}
